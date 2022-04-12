2357defff4372a9c75982f0f0f853c7e2b48d214
dasdias <das.dias6@gmail.com>
2022-04-12 01:30:55 +0100

    Library configurations for launch

--- src/__init__.py
+++ src/__init__.py
@@ -5,3 +5,28 @@ deflef_py is a DEF-LEF file parser for Python, written in Python.
 __version__ = '0.1.0'
 __author__  = 'Diogo André Silvares Dias'
 __email__   = 'das.dias@campus.fct.unl.pt'
+
+import warnings
+from src.util import (
+    Unsupported,
+    LefDecimal,
+    LefPoint,
+    LefPort,
+)
+from src.lef_data import (
+    LefLibrary, 
+    LefUnits, 
+    LefVia, 
+    LefViaRule, 
+    LefLayerCut, 
+    LefLayerRouting,
+    LefPin,
+)
+from src.lef_read import *
+from src.lef_write import *
+
+warnings.warn(
+    "[DELFEF] DEF data parsing is not yet supported.",
+    category = Unsupported,
+    stacklevel = 1
+)
